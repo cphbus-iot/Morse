541ece3e338d978eb40bd32ea8cab53a3341fdd0
Anders Kalhauge <anders@kalhauge.dk>
2017-10-02 10:46:57 +0200

    Initial

---
+++ examples/morsetest.ino
@@ -0,0 +1,14 @@
+#include <Morser.h>
+
+Morser morser(13);
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  morser.morse('S');
+  // put your main code here, to run repeatedly:
+
+}
